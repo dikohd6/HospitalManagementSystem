6d81a0e15f41a557cfa15f55a9c752246d688d3b
Dikran Kahiaian <kehiaiandikran@gmail.com>
2025-06-05 16:56:25 -0400

    bug fixes

--- connectdb.php
+++ connectdb.php
@@ -3,10 +3,8 @@ $dbhost = "localhost";
 $dbuser= "root";
 $dbpass = "cs3319";
 $dbname = "assign2db";
-$connection = mysqli_connect($dbhost, $dbuser,$dbpass,$dbname);
-if (mysqli_connect_errno()) {
-     die("database connection failed :" .
-     mysqli_connect_error() .
-     "(" . mysqli_connect_errno() . ")"
-         );
-    }
+try {
+    $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+} catch (mysqli_sql_exception $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
